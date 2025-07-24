85871f80d1f665ab7815cbad4df3eb0f9c7a4859
ayu-sh-30 <m.ayushyadav3003@gmail.com>
2025-07-24 23:37:57 +0530

    added a comment

--- src/middlewares/multer.middleware.js
+++ src/middlewares/multer.middleware.js
@@ -1,5 +1,5 @@
 import multer from "multer";
-
+// The disk storage engine gives you full control on storing files to disk
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
       cb(null, "./public/temp")
